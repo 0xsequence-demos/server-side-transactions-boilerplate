071a6c322367dbf3dad00f95f11dc6cda068d645
moskalyk <morgan.moskalyk@protonmail.ch>
2024-07-09 16:59:36 -0400

    update with erc721 vs erc1155 and sanitizing env variables and call params

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,38 @@ import { ethers } from 'ethers'
 import { Session } from '@0xsequence/auth'
 import { findSupportedNetwork, NetworkConfig } from '@0xsequence/network'
 
+const isValidEthereumAddress = (address: any) => {
+    return /^0x[a-fA-F0-9]{40}$/.test(address);
+};
+
+const isValidBoolean = (value: any) => {
+    return typeof value === 'boolean';
+};
+
+const isValidUint256 = (value: any) => {
+    return /^\d+$/.test(value) && BigInt(value) >= 0n;
+};
+
+async function getContractBytecode(contractAddress: string) {
+	// storage slot consistent location where proxies store the address of the logic contract they delegate to
+    const implementationSlot = "0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50";
+    try {
+        const storageValue1 = await provider.getStorageAt(contractAddress, implementationSlot);
+		return storageValue1
+    } catch (error) {
+        console.error("Error fetching bytecode:", error);
+		return null
+    }
+}
+
+const isNotValidERC1155ContractAddress = async (contractAddress: string) => {
+	return await getContractBytecode(contractAddress) != '0x000000000000000000000000ec9bf99632c3e97d2e461d16f48fc6fde8591191'
+}
+
+const isNotValidERC721ContractAddress = async (contractAddress: string) => {
+	return await getContractBytecode(contractAddress) != '0x000000000000000000000000f625720cdd63a65a5b7b31e7d0e64ae1ce08e52c'
+}
+
 const PORT = 3000
 const app = express()
 
@@ -19,32 +51,6 @@ const chainConfig: NetworkConfig = findSupportedNetwork(process.env.CHAIN_HANDLE
 const provider = new ethers.providers.StaticJsonRpcProvider({
 	url: chainConfig.rpcUrl
 })
-
-const callContract = async (address: string, tokenID: number): Promise<ethers.providers.TransactionResponse> => {
-	
-	const signer = await getSigner()
-	
-	// Standard interface for ERC1155 contract deployed via Sequence Builder
-	const collectibleInterface = new ethers.utils.Interface([
-		'function mint(address to, uint256 tokenId, uint256 amount, bytes data)'
-	])
-		
-	const data = collectibleInterface.encodeFunctionData(
-		'mint', [`${address}`, `${tokenID}`, "1", "0x00"]
-	)
-
-	const txn = {
-		to: process.env.COLLECTIBLE_CONTRACT_ADDRESS, 
-		data: data
-	}
-
-	try {
-		return await signer.sendTransaction(txn)
-	} catch (err) {
-		console.error(`ERROR: ${err}`)
-		throw err
-	}
-}
 
 const getSigner = async () => {
 	try {
@@ -62,11 +68,96 @@ const getSigner = async () => {
 	}
 }
 
+// check to ensure Project Access Key & EVM Private Key is valid
+getSigner()
+
+const callContract = async (evmAddress: string, contractAddress: string, isERC1155: boolean, tokenID: string, amount: string): Promise<ethers.providers.TransactionResponse> => {
+	
+	const signer = await getSigner()
+	
+	let collectibleInterface;
+	let data;
+
+	if(isERC1155){
+
+		// Standard interface for ERC1155 contract deployed via Sequence Builder
+		collectibleInterface = new ethers.utils.Interface([
+			'function mint(address to, uint256 tokenId, uint256 amount, bytes data)'
+		])
+
+		data = collectibleInterface.encodeFunctionData(
+			'mint', [`${evmAddress}`, `${tokenID}`, String(amount), "0x00"]
+		)
+
+	} else {
+		console.log('else')
+		
+		// Standard interface for ERC1155 contract deployed via Sequence Builder
+		collectibleInterface = new ethers.utils.Interface([
+			'function mint(address to, uint256 amount)'
+		])
+
+		data = collectibleInterface.encodeFunctionData(
+			'mint', [`${evmAddress}`, String(amount)]
+		)
+	}
+
+	const txn = {
+		to: contractAddress, 
+		data: data
+	}
+
+	try {
+		return await signer.sendTransaction(txn)
+	} catch (err) {
+		console.error(`ERROR: ${err}`)
+		throw err
+	}
+}
+
 app.post('/mint', async (req: any,res: any) => {
     try{
-        const {evmAddress, tokenID} = req.body
-        const result = await callContract(evmAddress,tokenID)
-        res.send({txHash: result.hash})
+        const { evmAddress, contractAddress, isERC1155, tokenID, amount } = req.body
+        
+		if (
+			!isValidEthereumAddress(evmAddress)
+		) 
+		{
+			return res.status(400).send({ error: "Invalid input parameters: 'evmAddress'" });
+		}
+
+		if(
+			!isValidBoolean(isERC1155)
+		)
+		{
+			return res.status(400).send({ error: "Invalid input parameters: 'isERC1155'" });
+		}
+
+		if(
+			!isValidUint256(tokenID)
+		)
+		{
+			return res.status(400).send({ error: "Invalid input parameters: 'tokenID'" });
+		}
+
+		if(
+			!isValidUint256(amount)
+		)
+		{
+			return res.status(400).send({ error: "Invalid input parameters: 'amount'" });
+		}
+
+		if(isERC1155 && await isNotValidERC1155ContractAddress(contractAddress)){
+			return res.status(400).send({ error: "Invalid input parameters: 'contractAddress'" });
+		} 
+		
+		if(!isERC1155 && await isNotValidERC721ContractAddress(contractAddress)){
+			return res.status(400).send({ error: "Invalid input parameters: 'contractAddress'" });
+		}
+
+		const result = await callContract(evmAddress, contractAddress, isERC1155, tokenID, String(amount))
+        
+		res.send({txHash: result.hash})
     }catch(err){
         console.log(err)
         res.sendStatus(500)
