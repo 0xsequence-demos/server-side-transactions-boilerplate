a277d3ae398e71e61329b6946ab1fc3da047d81b
moskalyk <morgan.moskalyk@protonmail.ch>
2024-07-26 10:26:21 -0400

    added tests

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,5 +1,14 @@
-const test = require('tape');
-const axios = require('axios');
+import test from 'tape';
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3000/mint';
+
+const validPayload = {
+  evmAddress: "0xe6eB28398CCBe46aA505b62b96822c2Ce8DAABf4",
+  contractAddress: "0x9f00671530137a433d5a775698094e5c68aae996",
+  isERC1155: false,
+  amount: 1
+};
 
 const checkServer = async () => {
   try {
@@ -26,10 +35,117 @@ const pollServer = async (interval = 2000) => {
 const runTests = async () => {
   await pollServer();
 
-  test('Tx Manager: Test', async (t) => {
-    t.ok(true, 'Server is running and test executed.');
+  test('Tx Manager: Valid ERC721 Minting', async (t) => {
+    try {
+      const response = await axios.post(BASE_URL, validPayload);
+      t.equal(response.status, 200, 'Should return 200 status');
+      t.ok(response.data.txHash, 'Should return a txHash');
+    } catch (error) {
+      t.fail('Failed valid ERC721 minting');
+    }
     t.end();
   });
+
+  test('Tx Manager: Bad EVM Address', async (t) => {
+    const payload = { ...validPayload, evmAddress: "invalid_address" };
+    try {
+      await axios.post(BASE_URL, payload);
+      t.fail('Should fail with bad evm address');
+    } catch (error) {
+      t.pass('Failed with bad evm address as expected');
+    }
+    t.end();
+  });
+
+  test('Tx Manager: No EVM Address', async (t) => {
+    const { evmAddress, ...payload } = validPayload;
+    try {
+      await axios.post(BASE_URL, payload);
+      t.fail('Should fail without evm address');
+    } catch (error) {
+      t.pass('Failed without evm address as expected');
+    }
+    t.end();
+  });
+
+  test('Tx Manager: Bad Contract Address', async (t) => {
+    const payload = { ...validPayload, contractAddress: "invalid_address" };
+    try {
+      await axios.post(BASE_URL, payload);
+      t.fail('Should fail with bad contract address');
+    } catch (error) {
+      t.pass('Failed with bad contract address as expected');
+    }
+    t.end();
+  });
+
+  test('Tx Manager: No Contract Address', async (t) => {
+    const { contractAddress, ...payload } = validPayload;
+    try {
+      await axios.post(BASE_URL, payload);
+      t.fail('Should fail without contract address');
+    } catch (error) {
+      t.pass('Failed without contract address as expected');
+    }
+    t.end();
+  });
+
+  test('Tx Manager: No isERC1155', async (t) => {
+    const { isERC1155, ...payload } = validPayload;
+    try {
+      await axios.post(BASE_URL, payload);
+      t.fail('Should fail without isERC1155');
+    } catch (error) {
+      t.pass('Failed without isERC1155 as expected');
+    }
+    t.end();
+  });
+
+  test('Tx Manager: Bad Amount', async (t) => {
+    const payload = { ...validPayload, amount: -1 };
+    try {
+      await axios.post(BASE_URL, payload);
+      t.fail('Should fail with bad amount');
+    } catch (error) {
+      t.pass('Failed with bad amount as expected');
+    }
+    t.end();
+  });
+
+  test('Tx Manager: No Amount', async (t) => {
+    const { amount, ...payload } = validPayload;
+    try {
+      await axios.post(BASE_URL, payload);
+      t.fail('Should fail without amount');
+    } catch (error) {
+      t.pass('Failed without amount as expected');
+    }
+    t.end();
+  });
+
+  test('Tx Manager: Valid ERC1155', async (t) => {
+    const payload = { ...validPayload, tokenID: 1025,contractAddress: "0x9c4ef3a17b8760169cc4e9c0f4f32f6757f87880", isERC1155: true };
+    try {
+      const response = await axios.post(BASE_URL, payload);
+      t.equal(response.status, 200, 'Should return 200 status');
+      t.ok(response.data.txHash, 'Should return a txHash');
+    } catch (error) {
+      t.fail('Failed with ERC1155');
+    }
+    t.end();
+  });
+
+  test('Tx Manager: ERC1155 with false isERC1155', async (t) => {
+    const payload = { ...validPayload, contractAddress: "0x9c4ef3a17b8760169cc4e9c0f4f32f6757f87880", isERC1155: false };
+    try {
+      await axios.post(BASE_URL, payload);
+      t.fail('Should fail with ERC1155 and false isERC1155');
+    } catch (error) {
+      t.pass('Failed with ERC1155 and false isERC1155 as expected');
+    }
+    t.end();
+  });
+
   // Exit process after all tests are complete
   test.onFinish(() => {
     console.log('All tests completed. Exiting process.');
@@ -39,5 +155,3 @@ const runTests = async () => {
 
 // Run the tests
 runTests();
-
-
